a4ebe321ea13e19714172f5fcd28bc0389ebf023
77saipooji <saipoojitha320@gmail.com>
2025-10-11 13:40:16 +0530

    Add files via upload

---
+++ twitterlit/posts/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.http import HttpResponseForbidden
+from .forms import RegisterForm, PostForm
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request,'posts/home.html',{'posts': posts})
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f"welcome, {user.username} ! youraccount was created succesfully")
+            return redirect('home')
+        else:
+                messages.error(request, "please correct the errors below.")
+    else:
+
+            form = RegisterForm()
+    return render(request, 'posts/register.html', {'form': form})
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+             post = form.save(commit=False)
+             post.user = request.user
+             post.save()
+             messages.success(request," Your post has been shared")
+             return redirect("home")
+    else:
+        form = PostForm()
+    return render(request, 'posts/create_post.html',{'form': form})
+@login_required
+def delete_post(requested, post_id):
+    post = post.abject.get(id=post_id)
+
+    if post.user != request.user:
+        return HttpResponseForbidden("you can only delete your own post.")
+
+    post.delete()
+    messages.success(request, "post deleted successfully.")
+    return redirect('home')
+
+def profile(requst,user):
+    user = get_object_or_404 (User, username=username)
+    posts = Post.object.filter(user=user).order_by('-created_at')
+    return "rendder"(requst, 'profile.html', {'profile_user':user,'posts':posts})
+
+
